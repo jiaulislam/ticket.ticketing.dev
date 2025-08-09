90a8ebbe32940643eb96104c9e83b449bd230fd7
jiaul.jibon <jiaul.jibon@strativ.se>
2025-08-09 23:09:05 +0600

    implementing consumer class

--- src/service/consumer.service.ts
+++ src/service/consumer.service.ts
@@ -1,22 +1,23 @@
-import { BaseConsumer } from "@jiaul.islam/common.ticketing.dev"
-
+import { BaseConsumer, ProducerMessage, Subject } from "@jiaul.islam/common.ticketing.dev";
 import { ConsumerGlobalAndTopicConfig } from "@confluentinc/kafka-javascript/types/kafkajs";
 
+interface TicketCreatedEvent {
+    id: string;
+    title: string;
+    price: number;
+}
 
-class TicketConsumer extends BaseConsumer {
-    protected config: ConsumerGlobalAndTopicConfig;
-    public topic: string;
-
+class TicketCreatedConsumer extends BaseConsumer<TicketCreatedEvent> {
     constructor(config: ConsumerGlobalAndTopicConfig, topic: string) {
-        super();
-        this.config = config;
-        this.topic = topic;
+        super(config, topic);
     }
 
-    // set the consumer's group ID, offset and initialize it
-    // config["group.id"] = "nodejs-group-1";
-    // config["auto.offset.reset"] = "earliest";
-    async consumeTicketEvents() {
-        await this.consume(this.topic, this.config);
+    handleMessage(message: ProducerMessage<TicketCreatedEvent>): void {
+        const { subject, data } = message;
+        if (subject == Subject.TICKET_CREATED) {
+            console.log(`Received event: ${subject}`);
+            console.log(`Ticket ID: ${data.id}, Title: ${data.title}, Price: ${data.price}`);
+            // Here you could add logic to process the ticket, e.g., save to DB
+        }
     }
 }
